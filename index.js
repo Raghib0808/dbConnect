b62d09a77117cc737198077ed7a9519f456c7c2c
Raghib <rahgib@gmail.com>
2025-07-16 01:39:57 +0530

    contribution

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const PORT = 8080;
 
 // setting up cors
+// checking for contributions
 // adding the .gitignore file to ignore node_modules and .env files
 app.use(express.json());
 
