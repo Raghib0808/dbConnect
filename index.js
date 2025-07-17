e4a90a132c1ee5704d81f9a812112fb5a97342d2
Raghib <raghibanis72@gmail.com>
2025-07-17 23:37:50 +0530

    env hide

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import db from './utils/db.js';
 dotenv.config();
 
 
 const app = express();
-// const PORT = process.env.PORT||3000;
-const PORT = 8080;
+const PORT = process.env.PORT||3000;
+// const PORT = 8080;
+// console.log(process.env.PORT);
 
 // setting up cors
 // checking for contributions (failed)
@@ -17,7 +19,7 @@ app.use(express.urlencoded({extended:true}))
 
 
 app.use(cors({
-  origin:['http://127.0.0.1:5500'],
+  origin:['http://127.0.0.1:5500',process.env.BASE_URL],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }))
@@ -29,8 +31,8 @@ app.get('/', (req, res) => {
 
 
 // console.log(process.env); 
-
-
+// connecting to the db
+db();
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,13 @@
 import mongoose from 'mongoose';
+
+const db=()=>{
+    mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log(`Connected to MongoDB at ${process.env.MONGO_URL}`);
+})
+.catch((err)=>{
+    console.log(`Error connecting to MongoDB: ${err.message}`);
+})
+}
+
+export default db;
